afbfb1c2ff37fdebbeb70c08276a9d4afafe3c89
Montse <mtschamorro@gmail.com>
2014-10-27 09:04:17 +0100

    creado clase persona y mth getage

---
+++ Person.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person(int ageOfPerson)
+    {
+        // initialise instance variables
+        age = ageOfPerson;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getAge()
+    {
+        // put your code here
+        return age ;
+    }
+}
